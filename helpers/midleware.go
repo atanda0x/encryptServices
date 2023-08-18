4b82255dc4b5c56bbfae4424fcade692a3600c40
atanda0x <atandanafiu21@gmail.com>
2023-08-17 23:14:15 -0700

    added logger

--- helpers/midleware.go
+++ helpers/midleware.go
@@ -4,7 +4,7 @@ import (
 	"context"
 	"time"
 
-	"github.com/go-kit/kit/log"
+	"github.com/go-kit/log"
 )
 
 // LoggingMiddleware wraps the log for incoming request
--- main.go
+++ main.go
@@ -5,29 +5,47 @@ import (
 	"net/http"
 	"os"
 
-	kitlog "github.com/go-kit/kit/log"
-
-	httptransport "github.com/go-kit/kit/transport/http"
-
 	"github.com/atanda0x/encryptString/helpers"
+	kitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	httptransport "github.com/go-kit/kit/transport/http"
+	kitlog "github.com/go-kit/log"
+	stdprometheus "github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
 	logger := kitlog.NewLogfmtLogger(os.Stderr)
+	fieldKeys := []string{"method", "error"}
+	requestCount := kitprometheus.NewCounterFrom(stdprometheus.CounterOpts{
+		Namespace: "encryption",
+		Subsystem: "my_service",
+		Name:      "request_count",
+		Help:      "Number of requests received.",
+	}, fieldKeys)
+	requestLatency :=
+		kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
+			Namespace: "encryption",
+			Subsystem: "my_service",
+			Name:      "request_latency_microseconds",
+			Help:      "Total duration of requests in microseconds.",
+		}, fieldKeys)
+
 	var svc helpers.EncryptService
 	svc = helpers.EncryptServiceInstance{}
 	svc = helpers.LoggingMiddleware{Logger: logger, Next: svc}
 
-	encryptHandler := httptransport.NewServer(helpers.MakeEncryptEndpoint(svc),
-		helpers.DecodeEncryptRequest,
-		helpers.EncodeResponse)
-
-	decryptHandler := httptransport.NewServer(helpers.MakeDecryptEndpoint(svc),
-		helpers.DecodeDecryptRequest,
-		helpers.EncodeResponse)
-
+	svc = helpers.InstrumentingMiddleware{RequestCount: requestCount,
+		RequestLatency: requestLatency, Next: svc}
+	encryptHandler :=
+		httptransport.NewServer(helpers.MakeEncryptEndpoint(svc),
+			helpers.DecodeEncryptRequest,
+			helpers.EncodeResponse)
+	decryptHandler :=
+		httptransport.NewServer(helpers.MakeDecryptEndpoint(svc),
+			helpers.DecodeDecryptRequest,
+			helpers.EncodeResponse)
 	http.Handle("/encrypt", encryptHandler)
 	http.Handle("/decrypt", decryptHandler)
-	log.Fatal(http.ListenAndServe(":8000", nil))
-
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
